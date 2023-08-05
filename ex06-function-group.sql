d87c51781687a18eb758db5600c4cafb5202eeb7
1emonac <jay.kang2@gmail.com>
2023-08-05 17:33:50 +0900

    Update ex06-function-group.sql

--- ex06-function-group.sql
+++ ex06-function-group.sql
@@ -13,3 +13,12 @@ SELECT AVG(salary) AS avg_salary,
             SUM(salary) AS total_salary
 FROM employees
 WHERE job_id LIKE '%REP%';
+
+-- COUNT() 함수 - null 값이 아닌 모든 행의 개수를 반환합니다.
+SELECT COUNT(*) AS total_employees
+FROM employees
+WHERE department_id = 50;
+
+SELECT COUNT(commission_pct) AS non_null_commission_count
+FROM employees
+WHERE department_id = 80;
