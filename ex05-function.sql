8381b7e79ddf84ca2aaea9f83325765ec7c65d9b
1emonac <jay.kang2@gmail.com>
2023-08-05 15:33:25 +0900

    Update ex05-function.sql

--- ex05-function.sql
+++ ex05-function.sql
@@ -60,3 +60,37 @@ SELECT TRIM(' ' FROM ' Hello World ') FROM dual; -- 공백 제거할 때 많이 사용됨
 -- ROUND() - 지정된 소수점 자릿수로 값을 반올림합니다.
 SELECT ROUND(45.926, 2) FROM dual;
 
+-- TRUNC() - 지정된 소수점 자릿수로 값을 전달합니다.
+SELECT TRUNC(45.926, 2) FROM dual;
+
+-- MOD() - 나눈 나머지를 반환합니다.
+SELECT MOD(1600, 300) FROM dual;
+
+-- CEIL() - 주어진 숫자를 올림하여 정수로 반환합니다.
+SELECT CEIL(45.2) FROM dual;
+
+
+-- 날짜 함수
+-- SYSDAYE - 현재 날짜와 시간을 얻을 수 있는 pseudo-column
+SELECT SYSDATE FROM dual;
+
+/*
+날짜 연산
+        날짜에 숫자를 더하거나 빼서 결과 날짜 값을 구합니다.
+        두 날짜 사이의 일수를 알아내기 위해 빼기연산을 합니다.
+*/
+SELECT last_name, (sysdate - hire_date) / 7 AS WEEKS
+FROM employees
+WHERE department_id = 90;
+
+/*
+날짜 조작 함수
+        MONTHS_BETWEEN(date1, date2) : 두 날짜 간의 월 수를 계산합니다.
+        ADD_MONTHS(date, n) : 날짜에 n개월을 추가합니다.
+        NEXT_DAY(date, day_of_week) : 지정된 날짜의 다음으로 주어진 요일이 나오는 날짜를 계산합니다. (1:월요일 ~ 7:일요일)
+        LAST_DAY(date) : 주어진 월의 마지막 날짜를 반환합니다.
+        ROUND(date, format) : 날짜를 지정된 형식으로 반올림합니다.
+        TRUNC(date, format) : 날짜를 지정된 형식으로 절삭합니다.
+*/
+SELECT MONTHS_BETWEEN(TO_DATE('2017-12-22', 'YYYY-MM-DD'), TO_DATE('2017-05-22', 'YYYY-MM-DD')) FROM dual;
+SELECT ADD_MONTHS(TO_DATE('2022-12-16', 'YYYY-MM-DD'), 1) FROM dual;
