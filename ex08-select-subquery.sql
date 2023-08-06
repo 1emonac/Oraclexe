266f9547c82cdb5da3dba4b5f1e41ee7dbfcf9cb
1emonac <jay.kang2@gmail.com>
2023-08-06 17:30:30 +0900

    Update ex08-select-subquery.sql

--- ex08-select-subquery.sql
+++ ex08-select-subquery.sql
@@ -28,7 +28,29 @@ WHERE salary = (SELECT MIN(salary) FROM employees);
                 > ALL은 최대값보다 큼을 의미합니다.
                 < ALL은 최소값보다 작음을 의미합니다.                
 */
+-- ANY
 SELECT employee_id, last_name, job_id, salary
 FROM employees
 WHERE salary < ANY(SELECT salary FROM employees WHERE job_id = 'IT_PROG')
 AND job_id <> 'IT_PROG';
+
+-- ALL
+SELECT employee_id, last_name, job_id, salary
+FROM employees
+WHERE salary > ALL(SELECT salary FROM employees WHERE job_id = 'IT_PROG') -- 최대값(9000)보다 큰 것들을 보여줌
+AND job_id <> 'IT_PROG';
+
+-- IN
+SELECT employee_id, last_name, job_id, salary
+FROM employees
+WHERE salary IN(SELECT salary FROM employees WHERE job_id = 'IT_PROG') -- 최대값(9000)보다 큰 것들을 보여줌
+AND job_id <> 'IT_PROG';
+
+/*
+EXISTS 연산자
+        subquery에서 최소한 한 개의 행을 반환하면 TRUE로 평가됩니다.
+*/
+SELECT * FROM departments
+WHERE NOT EXISTS
+                (SELECT * FROM employees
+                WHERE employees.department_id = departments.department_id);
