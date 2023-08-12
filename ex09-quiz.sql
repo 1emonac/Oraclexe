a1ec631edee14b86c6bf63a5a39816c23c309adf
1emonac <jay.kang2@gmail.com>
2023-08-12 16:25:04 +0900

    Revert "Update ex09-quiz.sql"
    This reverts commit 056b39de838682515c15eedb1ad836d4e34f6808.

--- ex09-quiz.sql
+++ ex09-quiz.sql
@@ -2,85 +2,20 @@
 파일명: ex09-quiz.sql
 */
 
----- 1. 각 직원의 성(last_name)과 해당 직원의 매니저인 직원의 성(last_name) 조회하기
-SELECT worker.last_name AS worker_name, manager.last_name AS manager_name
-FROM employees worker JOIN employees manager 
-ON worker.manager_id = manager.employee_id;
-
----- 2. 각 직원의 성(last_name)과 해당 직원의 부서 이름(department_name) 조회하기
-SELECT e.last_name, d.department_name
-FROM employees e
-JOIN departments d
-ON e.department_id=d.department_id;
-
----- 3. 각 부서의 이름(department_name)과 해당 부서의 평균 급여(avg_salary) 조회하기
-SELECT d.department_name, AVG(e.salary) AS avg_salary
-FROM employees e JOIN departments d
-ON e.department_id = d.department_id
-GROUP BY department_name;
-
----- 4. 각 부서의 이름(department_name)과 해당 부서의 최대 급여(max_salary) 조회하기
-SELECT d.department_name, MAX(e.salary) AS max_salary
-FROM employees e JOIN departments d
-ON e.department_id = d.department_id
-GROUP BY department_name;
-
+-- 1. 각 직원의 성(last_name)과 해당 직원의 매니저인 직원의 성(last_name) 조회하기
+-- 2. 각 직원의 성(last_name)과 해당 직원의 부서 이름(department_name) 조회하기
+-- 3. 각 부서의 이름(department_name)과 해당 부서의 평균 급여(avg_salary) 조회하기
+-- 4. 각 부서의 이름(department_name)과 해당 부서의 최대 급여(max_salary) 조회하기
 -- 5. 각 직원의 성(last_name)과 해당 직원이 속한 부서의 최소 급여(min_salary) 조회하기
-SELECT last_name, MIN(salary) AS min_salary
-FROM employees
-GROUP BY department_id, last_name;
-
-SELECT oe.last_name, od.min_salary
-FROM employees oe
-JOIN (SELECT d.department_id, MIN(e.salary) AS min_salary
-        FROM departments d JOIN employees e 
-        ON d.department_id = e.department_id
-        GROUP BY d.department_id) od
-ON oe.department_id = od.department_id;
-
-
 -- 6. 각 부서의 이름(department_name)과 해당 부서에 속한 직원 중 가장 높은 급여(highest_salary) 조회하기
-SELECT department_name, max(salary) AS highest_salary
-FROM employees
-JOIN departments USING(department_id)
-GROUP BY department_name;
-
-SELECT d.department_name, MAX(e.salary) AS highest_salary
-FROM employees e JOIN departments d
-ON e.department_id = d.department_id
-GROUP BY department_name;
-
-
 -- 7. 각 직원의 성(last_name)과 해당 직원의 매니저의 성(last_name) 및 부서 이름(department_name) 조회하기
-SELECT emp.last_name AS emp_name, mgr.last_name AS mgr_name, d.department_name AS department_name
-FROM employees emp, employees mgr, departments d
-WHERE emp.department_id = d.department_id
-AND emp.manager_id = mgr.employee_id;
-
-SELECT e.last_name, m.last_name, d.department_name
-FROM employees e
-JOIN employees m ON e.manager_id = m.employee_id
-JOIN departments d ON e.department_id = d.department_id;
-
-
 -- 8. 각 직원의 성(last_name)과 해당 직원이 속한 부서의 매니저의 성(last_name) 조회하기
-
-
--- 9. 직원들 중에서 급여(salary)가 10000 이상인 직원들의 성(last_name)과 해당 직원의 부서 이름(department_name) 조회하기
-SELECT e.last_name, d.department_name
-FROM employees e JOIN departments d
-ON e.department_id = d.department_id
-WHERE salary >= 10000
-ORDER BY e.salary DESC;
-
+-- 9. 각 직원의 성(last_name)과 해당 직원의 보고 상사가 있는 경우 보고 상사의 성(last_name) 조회하기
+-- 10. 직원들 중에서 급여(salary)가 10000 이상인 직원들의 성(last_name)과 해당 직원의 부서 이름(department_name) 조회하기
 /*
-10.
+11.
 각 부서의 이름(department_name), 해당 부서의 매니저의 ID(manager_id)와 매니저의 성(last_name),
 직원의 ID(employee_id), 직원의 성(last_name), 그리고 해당 직원의 급여(salary) 조회하기.
 직원들의 급여(salary)가 해당 부서의 평균 급여보다 높은 직원들을 조회합니다.
 결과는 부서 이름과 직원의 급여가 높은 순으로 정렬됩니다.
 */
-SELECT d.department_name, d.manager_id, m.last_name
-FROM departments d
-JOIN employees m ON d.manager_id = m.employee_id0
-;
